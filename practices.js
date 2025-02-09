9883605128f59317c02a19bf0e1e6671aef417ad
Naomi Shvayev <naomishvayev@Naomis-MacBook-Air.local>
2025-02-09 18:17:53 +0200

    cleaner

--- practices.js
+++ practices.js
@@ -1,5 +1,15 @@
-function change_title () { 
-    
-    h1.innerHTML = "goodbye world!";
-
-} 
+function change_title () { h1.innerHTML = "goodbye world!"; } 
+function change_title(){ document.getElementsByTagName('h1')[0].textContent = 'you clicked!'; }
+function dissapear(){ document.getElementById('lorem').style.display = 'none'; }
+function darkmode(){
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'papayawhip';
+    document.getElementsByTagName('h1')[0].style.color = 'papayawhip';
+    document.getElementsByTagName('img')[0].style.display = 'none';
+    document.getElementsByTagName('img')[1].style.display = 'block';           }
+function lightmode(){
+    document.body.style.backgroundColor = 'papayawhip';
+    document.body.style.color = 'black';
+    document.getElementsByTagName('h1')[0].style.color = 'black';
+    document.getElementsByTagName('img')[1].style.display = 'none'; 
+    document.getElementsByTagName('img')[0].style.display = 'block';           }
